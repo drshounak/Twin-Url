07c7a8d3d93c0a2fe1cd09e5f8f068e57345e620
drshounak <139616389+drshounak@users.noreply.github.com>
2024-04-03 14:03:20 +0530

    Update workers.js
    Signed-off-by: drshounak <139616389+drshounak@users.noreply.github.com>

--- workers.js
+++ workers.js
@@ -268,6 +268,7 @@ async function serveDeletePage(env) {
       <head>
         <meta charset="UTF-8">
         <title>Delete Redirect</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
           body {
             font-family: Arial, sans-serif;
@@ -276,7 +277,7 @@ async function serveDeletePage(env) {
             justify-content: center;
             align-items: center;
             height: 100vh;
-            margin: 0;
+            margin: 7px;
           }
 
           .container {

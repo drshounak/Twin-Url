6efa43fe65864f3192bb7f8ee512156e4aca0aff
drshounak <139616389+drshounak@users.noreply.github.com>
2025-02-22 00:22:06 +0530

    Create utils.js
    Signed-off-by: drshounak <139616389+drshounak@users.noreply.github.com>

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export function generateRandomString(length) {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
